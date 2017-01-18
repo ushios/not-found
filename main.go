073675a9935bf3d7f2c42443bf0aed2c1988f51b
Ushio Shugo <ushio.s@gmail.com>
2017-01-18 14:14:41 +0900

    Remove health check

--- main.go
+++ main.go
@@ -4,34 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"path/filepath"
 )
 
 const (
-	DefaultPort            = 80
-	DefaultHelathCheckPath = "/healthcheck"
+	DefaultPort = 80
 )
 
 var (
-	port        int
-	healthcheck string
+	port int
 )
 
 func main() {
 	flag.IntVar(&port, "port", DefaultPort, "http server port")
 	flag.IntVar(&port, "p", DefaultPort, "http server port")
-	flag.StringVar(&healthcheck, "healthcheck", DefaultHelathCheckPath, "health check path (return http status code 200)")
-	flag.StringVar(&healthcheck, "c", DefaultHelathCheckPath, "health check path (return http status code 200)")
 	flag.Parse()
 
-	healthcheck = filepath.Join("/", healthcheck)
-
-	fmt.Println("listen port:       ", port)
-	fmt.Println("health check path: ", healthcheck)
-
-	http.HandleFunc(healthcheck, func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "OK")
-	})
+	fmt.Println("listen port:", port)
 
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
